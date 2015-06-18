d17d2ad13075c879a79a45c5979b22bdb1a8d43a
ubergesundheit <ubergesundheit@googlemail.com>
2015-06-19 00:44:34 +0200

    change adresses, prepare for neighbor devices

--- EnOceanSpy.c
+++ EnOceanSpy.c
@@ -35,7 +35,9 @@ main(int argc, char *argv[]) {
   url = getenv(ENV_URL);
 
   char faft_addr[] = "01005170";
-  char faft_addr2[] = "01a05170";
+  char faft_addr2[] = "0088271E";
+  char faft_collection = "tmps_out";
+  char faft2_collection = "tmps_in1";
 
   // Check content of args
   if ((strcmp(device, "/dev/ttyUSB0") != 0) && (strcmp(device, "/dev/ttyAMA0") !=0) ) {
@@ -92,67 +94,71 @@ main(int argc, char *argv[]) {
 
         char address[8];
         char collection[9];
+        int is_my_device = 0;
         sprintf(address, "%02X%02X%02X%02X", rx_buffer[11],rx_buffer[12], rx_buffer[13], rx_buffer[14]);
 
         if(strcmp(faft_addr, address) == 0) {
-          strcpy(collection, "tmps_out");
+          is_my_device = 1;
+          strcpy(collection, faft_collection);
         }
         if(strcmp(faft_addr2, address) == 0) {
-          strcpy(collection, "tmps_in0");
+          is_my_device = 1;
+          strcpy(collection, faft2_collection);
         }
-        struct tm *utc;
-        time_t t;
-        t = time(NULL);
-        utc = localtime(&t);
+        if(is_my_device == 1) {
+          struct tm *utc;
+          time_t t;
+          t = time(NULL);
+          utc = localtime(&t);
 
-        char dt[80];
-        strftime(dt, 80, "%Y-%m-%dT%H:%M:%S%z", utc);
+          char dt[80];
+          strftime(dt, 80, "%Y-%m-%dT%H:%M:%S%z", utc);
 
-        double volt, hum, temp, volt_multiply_by;
+          double volt, hum, temp, volt_multiply_by;
 
-        // volt
-        volt_multiply_by = 0.0258039;
-        volt = volt_multiply_by * rx_buffer[7];
+          // volt
+          volt_multiply_by = 0.0258039;
+          volt = volt_multiply_by * rx_buffer[7];
 
-        // humidity
-        hum = (double)rx_buffer[8] / 2.5;
+          // humidity
+          hum = (double)rx_buffer[8] / 2.5;
 
-        // temperature
-        temp = 0.32 * (double)rx_buffer[9] - 20.0;
+          // temperature
+          temp = 0.32 * (double)rx_buffer[9] - 20.0;
 
-        char json[200];
+          char json[200];
 
-        sprintf(json, "{\"collection\":\"%s\",\"timestamp\":\"%s\",\"data\":{\"temp\":%.1f,\"hum\":%.1f,\"batt\":%.1f,\"raw_bytes\":\"%02X%02X%02X\",\"addr\":\"%s\"}}", collection, dt, temp, hum, volt, rx_buffer[7],rx_buffer[8], rx_buffer[9], address);
-        printf("%s\n", json);
+          sprintf(json, "{\"collection\":\"%s\",\"timestamp\":\"%s\",\"data\":{\"temp\":%.1f,\"hum\":%.1f,\"batt\":%.1f,\"raw_bytes\":\"%02X%02X%02X\",\"addr\":\"%s\"}}", collection, dt, temp, hum, volt, rx_buffer[7],rx_buffer[8], rx_buffer[9], address);
+          printf("%s\n", json);
 
-        // init curl
-        CURL *curl;
-        CURLcode res;
+          // init curl
+          CURL *curl;
+          CURLcode res;
 
-        curl_global_init(CURL_GLOBAL_DEFAULT);
+          curl_global_init(CURL_GLOBAL_DEFAULT);
 
-        curl = curl_easy_init();
-        if(curl) {
-          struct curl_slist *headers = NULL;
-          headers = curl_slist_append(headers, "Accept: application/json");
-          headers = curl_slist_append(headers, "Content-Type: application/json");
-          headers = curl_slist_append(headers, "charsets: utf-8");
-          curl_easy_setopt(curl, CURLOPT_URL, url);
-          curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-          curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json);
-          res = curl_easy_perform(curl);
-          /* Check for errors */
-          if (res != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-            curl_easy_strerror(res));
+          curl = curl_easy_init();
+          if(curl) {
+            struct curl_slist *headers = NULL;
+            headers = curl_slist_append(headers, "Accept: application/json");
+            headers = curl_slist_append(headers, "Content-Type: application/json");
+            headers = curl_slist_append(headers, "charsets: utf-8");
+            curl_easy_setopt(curl, CURLOPT_URL, url);
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json);
+            res = curl_easy_perform(curl);
+            /* Check for errors */
+            if (res != CURLE_OK) {
+              fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+            }
+
+            /* always cleanup */
+            curl_easy_cleanup(curl);
           }
 
-          /* always cleanup */
-          curl_easy_cleanup(curl);
+          curl_global_cleanup();
         }
-
-        curl_global_cleanup();
-
       }
     }
   }
